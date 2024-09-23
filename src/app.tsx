d3cef9b216c62da6084dcc39a362a89a4aefb1d6
Marek Klier <marek.klier.personal@gmail.com>
2024-09-23 23:36:58 +0200

    feat: add board component, handle game flow and score board (#3)
    Co-authored-by: Marek Klier <marekklier@pop-os.home>

--- src/app.tsx
+++ src/app.tsx
@@ -8,11 +8,14 @@ import { useAppState } from "@contexts/use-app-state";
 export function App() {
   const { appState } = useAppState();
 
+  const showBoard =
+    appState === AppState.IN_PROGRESS || appState === AppState.COMPLETED;
+
   return (
     <Grid gap="8" className="container mx-auto py-10 px-8  auto-rows-min">
       <Heading size="3xl">Who is the unlucky one</Heading>
       {appState === AppState.SETUP && <PeopleSetup />}
-      {appState === AppState.IN_PROGRESS && <Board />}
+      {showBoard && <Board />}
     </Grid>
   );
 }
